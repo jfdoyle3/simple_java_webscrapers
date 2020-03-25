c5231ceb34af206984104ea6c5ac050cb0611aa3
Jim Doyle <indellibleink@gmail.com>
2020-03-25 11:04:14 -0400

    2020-03-25 11:04

--- google_scraper/src/google_scraper/console_scraper.java
+++ google_scraper/src/google_scraper/console_scraper.java
@@ -2,6 +2,7 @@ package google_scraper;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.IOException;
 import java.net.URLEncoder;
@@ -9,16 +10,31 @@ import java.net.URLEncoder;
 public class console_scraper {
 
 	//Scrape: https://www.google.com/search?q=apple
-    //tag query: v:h3.r a      my guess for me: div.r a
+    //tag query: h3.r a
+	
+	//Declare a Variable
 	public static final String USER_AGENT="Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0";
 	
 	public static void main(String[] args) throws IOException {
 		
+		//User Query
 		final String query="apple";
 		
+		//Download requested page and append user query to it.
 		final Document page=Jsoup.connect("https://www.google.com/search?q="+URLEncoder.encode(query,"UTF-8")).userAgent(USER_AGENT).get();
 		
-		System.out.println(page.outerHtml());
+		//Iterate and Output to console the Title and URL link of query.
+		for (Element searchResult : page.select("h3.r a")) {
+			
+			//Get Text from Tag
+			final String title=searchResult.text();
+			
+			//Get URL from Tag
+			final String url=searchResult.attr("href");
+			
+			//Display to console
+			System.out.println(title + " >>---> " + url );
+		}
 
 	}
 }
