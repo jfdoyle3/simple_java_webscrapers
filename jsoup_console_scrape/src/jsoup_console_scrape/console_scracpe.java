59ea7c753ca4a39a9b6212adc18a339dc11403ea
Jim Doyle <indellibleink@gmail.com>
2020-03-25 09:41:02 -0400

    A simple console scraper with jsoup.

--- jsoup_console_scrape/src/jsoup_console_scrape/console_scracpe.java
+++ jsoup_console_scrape/src/jsoup_console_scrape/console_scracpe.java
@@ -1,5 +1,29 @@
 package jsoup_console_scrape;
 
-public class console_scracpe {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.IOException;
+
+public class console_scracpe {
+	
+	public static void main(String[] args) throws IOException {
+	
+	//Downloads the html from wikipedia and parses it
+	final Document doc = Jsoup.connect("https://en.wikipedia.org/").get();
+	
+	//Selects a bunch of tags
+	final Elements newsHeadlines = doc.select("#mp-itn b a");
+	
+	//Outputs to console the headlines from page
+	for (Element headline : newsHeadlines) {
+		System.out.println(headline.text());
+	}
+	System.out.println("\n\n--------------------------------------------------------\n\n");
+	
+	System.out.println(doc.outerHtml());
+  }	
 }
+
