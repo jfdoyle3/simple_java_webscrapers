f2ad2f4415d3f3e26d21a4ce74fe0ee5d54743ec
Jim Doyle <indellibleink@gmail.com>
2020-03-26 11:42:35 -0400

    2020-03-26 11:42

--- unirest_scraper/src/unirest_scraper/console_scraper.java
+++ unirest_scraper/src/unirest_scraper/console_scraper.java
@@ -1,9 +1,15 @@
 package unirest_scraper;
 
+
+
+
 public class console_scraper {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		
+		Unirest.get("http://htpbin.org/get").asString();
+		
+		System.out.println(response.getBody());
 
 	}
 
