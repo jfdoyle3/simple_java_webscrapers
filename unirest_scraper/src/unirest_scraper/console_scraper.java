43158195e473cb896ea6d84697cde6a428af8b8a
Jim Doyle <indellibleink@gmail.com>
2020-03-26 11:57:35 -0400

    2020-03-26 11:57

--- unirest_scraper/src/unirest_scraper/console_scraper.java
+++ unirest_scraper/src/unirest_scraper/console_scraper.java
@@ -1,13 +1,14 @@
 package unirest_scraper;
 
-
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
 
 
 public class console_scraper {
 
 	public static void main(String[] args) throws Exception {
 		
-		Unirest.get("http://htpbin.org/get").asString();
+		final HttpResponse<String> response=Unirest.get("http://httpbin.org/get").asString();
 		
 		System.out.println(response.getBody());
 
