0ec63c442bc353618be4edc11b027ba8836c4dc9
Jim Doyle <indellibleink@gmail.com>
2020-03-29 16:44:10 -0400

    2020-03-29 16:44

--- peoplefinder-scraper/src/main/PeopleFinderScraper.java
+++ peoplefinder-scraper/src/main/PeopleFinderScraper.java
@@ -1,10 +1,15 @@
 package main;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+
 public class PeopleFinderScraper {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception{
+	
 
+		final HttpResponse<String> response=Unirest.get("https://www.peoplefinders.com/api/widget/widgets?firstName=sam&lastName=smith&city=new-york&state=ny&totalRecords=10").asString();
+		System.out.println(response.getBody());
 	}
 
 }
