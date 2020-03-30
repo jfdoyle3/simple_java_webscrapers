c86acafced046724065626d183674cd795a4fc76
Jim Doyle <indellibleink@gmail.com>
2020-03-30 07:14:11 -0400

    2020-03-30 07:14

--- peoplefinder-scraper/src/main/PeopleFinderScraper.java
+++ peoplefinder-scraper/src/main/PeopleFinderScraper.java
@@ -1,5 +1,9 @@
 package main;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 
@@ -18,7 +22,18 @@ public class PeopleFinderScraper {
 												   .queryString("city",city)
 												   .queryString("start", state)
 												   .asString();
-		System.out.println(response.getBody());
+		//System.out.println(response.getBody());
+		
+		final Document htmlSnippet=Jsoup.parseBodyFragment(response.getBody());
+		
+		for (Element peopleResult : htmlSnippet.select("div.row > div.col-md-12 > div.row > div.col-md-11")) {
+			System.out.println(peopleResult.child(0).text());
+			/*
+			 * final String name=peopleResult.child(0).text(); final String
+			 * age=peopleResult.child(1).text();
+			 */
+			
+			}
 	}
 
 }
