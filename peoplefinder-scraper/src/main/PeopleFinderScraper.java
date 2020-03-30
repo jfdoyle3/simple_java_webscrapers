57d4b39f716f9a17214519b80c3fabb30a150635
Jim Doyle <indellibleink@gmail.com>
2020-03-30 06:59:10 -0400

    2020-03-30 06:59

--- peoplefinder-scraper/src/main/PeopleFinderScraper.java
+++ peoplefinder-scraper/src/main/PeopleFinderScraper.java
@@ -7,8 +7,17 @@ public class PeopleFinderScraper {
 
 	public static void main(String[] args) throws Exception{
 	
-
-		final HttpResponse<String> response=Unirest.get("https://www.peoplefinders.com/api/widget/widgets?firstName=sam&lastName=smith&city=new-york&state=ny&totalRecords=10").asString();
+		final String lastName="smith";
+		final String firstName="same";
+		final String city="New York";
+		final String state="ny";
+			
+		final HttpResponse<String> response=Unirest.get("https://www.peoplefinders.com/api/widget/widgets?totalRecords=10")
+												   .queryString("firstName", firstName)
+												   .queryString("lastName", lastName)
+												   .queryString("city",city)
+												   .queryString("start", state)
+												   .asString();
 		System.out.println(response.getBody());
 	}
 
