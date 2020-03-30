becd44b221885aa79d4d9191221c64a6cdc6f480
Jim Doyle <indellibleink@gmail.com>
2020-03-30 08:59:10 -0400

    2020-03-30 08:59

--- YellowPages_scraper/src/YellowPagesScraper/console_scraper.java
+++ YellowPages_scraper/src/YellowPagesScraper/console_scraper.java
@@ -29,22 +29,19 @@ public class console_scraper {
 		  final Document htmlSnippet = Jsoup.parseBodyFragment(response.getBody());
 		  System.out.println("Got Snippet");
 		 // System.out.println(htmlSnippet.outerHtml());
-		 
-		// for (Element peopleResult : htmlSnippet.select("div.row > div.col-md-12 >
-		// div.row > div.col-md-11")) {
+{
 		
-		  for (Element bikeShops : htmlSnippet.select("div.info")) {
+		  for (Element bikeShops : htmlSnippet.select("div.info")) 
+		  {
 		  //System.out.println("results:"); System.out.println(peopleResult.outerHtml());
 		  //System.out.println(bikeShops.child(1).text());
 		  
 		  final String businnessName=bikeShops.child(0).text(); 
 		
 		  System.out.println(businnessName);
-
-		  
-		  
-		  }
+		   }
 		 
 		System.out.println("end of line");
+		}
 	}
 }
